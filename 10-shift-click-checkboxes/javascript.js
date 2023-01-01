9c3035ffab4c0a61ecacfe5a863f9c8e2fa3003e
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-01-01 19:20:16 +0000

    Progress on Shift-Click Checkboxes

--- 10-shift-click-checkboxes/javascript.js
+++ 10-shift-click-checkboxes/javascript.js
@@ -1,12 +1,21 @@
 const checkbox = document.querySelectorAll("input");
 
 const checkboxArray = Array.from(checkbox);
-
 console.log(checkboxArray);
 
-checkbox.forEach(checkbox => checkbox.addEventListener("click", shiftClick));
+checkboxArray.forEach(checkbox => checkbox.addEventListener("click", shiftClick));
 
 function shiftClick(e) {
     if (!e.shiftKey) return;
-    if (e.shiftKey === true) console.log("You shift clicked");
+    if (e.shiftKey === true) {
+
+        console.log(`You shift clicked`);
+
+        const checked = (checkbox) => checkbox.checked;
+        const selectionStart = checkboxArray.findIndex(checked);
+        const selectionEnd = checkboxArray.findLastIndex(checked);
+
+        console.log(selectionStart);
+        console.log(selectionEnd);
+    }
 }
