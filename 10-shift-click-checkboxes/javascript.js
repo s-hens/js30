77cb44271c95ed20861aed23d436f65bd6f3fb0f
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-01-01 20:25:20 +0000

    Complete Shift-Click Checkboxes tutorial

--- 10-shift-click-checkboxes/javascript.js
+++ 10-shift-click-checkboxes/javascript.js
@@ -1,3 +1,5 @@
+/* My solution
+
 const checkbox = document.querySelectorAll("input");
 
 const checkboxArray = Array.from(checkbox);
@@ -6,7 +8,7 @@ checkboxArray.forEach(checkbox => checkbox.addEventListener("click", shiftClick)
 
 function shiftClick(e) {
     if (!e.shiftKey) return;
-    if (e.shiftKey === true) {
+    if (e.shiftKey) {
         const checked = (checkbox) => checkbox.checked;
         const selectionStart = checkboxArray.findIndex(checked);
         const selectionEnd = checkboxArray.findLastIndex(checked);
@@ -14,3 +16,27 @@ function shiftClick(e) {
         selection.forEach(checkbox => checkbox.checked = true);
     }
 }
+*/
+
+//Wes' Solution
+
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+        console.log(checkbox);
+        if (checkbox === this || checkbox === lastChecked) {
+            inBetween = !inBetween;
+        }
+        if (inBetween) {
+            checkbox.checked = true;
+        }
+});}
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
