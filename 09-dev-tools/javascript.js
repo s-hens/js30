d3535e55924b98f0d9a9c9af9081df96fb583f81
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-01-01 16:48:11 +0000

    Initial setup for Project 9: Dev Tools

---
+++ 09-dev-tools/javascript.js
@@ -0,0 +1,31 @@
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+
+function makeGreen() {
+    const p = document.querySelector('p');
+    p.style.color = '#BADA55';
+    p.style.fontSize = '50px';
+}
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// Warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// Clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// Counting
+
+// Timing
