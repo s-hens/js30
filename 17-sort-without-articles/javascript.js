8cccaf250ef2b501d43c5485f3cdbe362048e20e
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-01-17 15:45:56 +0000

    Progress on Sort Without Articles

--- 17-sort-without-articles/javascript.js
+++ 17-sort-without-articles/javascript.js
@@ -1,1 +1,29 @@
-const bands = ["The Strokes", "The Velvet Underground", "Fleetwood Mac", "Arctic Monkeys", "Tame Impala", "The Beatles", "Bauhaus", "The Shins", "Paramore", "The Zombies"];
+const bandList = document.querySelector("#bands");
+
+const bands = ["The Strokes", "The Velvet Underground", "Fleetwood Mac", "Arctic Monkeys", "Tame Impala", "The Beatles", "Bauhaus", "The Shins", "Nirvana", "A Tribe Called Quest"];
+let bandsWithoutArticles = [];
+let bandsSorted = [];
+
+bands.forEach(band => sort(band));
+
+function sort(band) {
+    let withoutArticle;
+    if (band.startsWith("A ")) {
+        withoutArticle = band.slice(2);
+    } else if (band.startsWith("The ")) {
+        withoutArticle = band.slice(4);
+    } else {
+        withoutArticle = band;
+    }
+    bandsWithoutArticles.push(withoutArticle);
+    bandsWithoutArticles.sort();
+}
+
+bandsWithoutArticles.forEach(bandWithoutArticle => replaceArticles(bandWithoutArticle));
+
+function replaceArticles(bandWithoutArticle) {
+    newName = bands.find(element => element.includes(bandWithoutArticle));
+    bandsSorted.push(newName);
+}
+
+bandsSorted.forEach(bandSorted => bandList.innerHTML = `${bandList.innerHTML}<li>${bandSorted}</li>`)
