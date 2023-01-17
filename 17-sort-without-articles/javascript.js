9d6458d80b34afefecc5412a592acde1393b3b67
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-01-17 15:56:14 +0000

    Complete Sort Without Articles tutorial

--- 17-sort-without-articles/javascript.js
+++ 17-sort-without-articles/javascript.js
@@ -1,6 +1,7 @@
-const bandList = document.querySelector("#bands");
-
 const bands = ["The Strokes", "The Velvet Underground", "Fleetwood Mac", "Arctic Monkeys", "Tame Impala", "The Beatles", "Bauhaus", "The Shins", "Nirvana", "A Tribe Called Quest"];
+
+/* My Solution
+
 let bandsWithoutArticles = [];
 let bandsSorted = [];
 
@@ -26,4 +27,20 @@ function replaceArticles(bandWithoutArticle) {
     bandsSorted.push(newName);
 }
 
+const bandList = document.querySelector("#bands");
+
 bandsSorted.forEach(bandSorted => bandList.innerHTML = `${bandList.innerHTML}<li>${bandSorted}</li>`)
+*/
+
+// Wes' Solution ... much more concise
+
+function strip(bandName) {
+    return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+  
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+  
+document.querySelector('#bands').innerHTML =
+    sortedBands
+        .map(band => `<li>${band}</li>`)
+        .join('');
